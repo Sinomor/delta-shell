e8c5a78260f5fa8bbad2cd69157cf98916f4ed44
sinomor <scarletweel@gmail.com>
2025-08-11 16:45:47 +0300

    feat: fix image sizing in clipboard

--- widgets/launcher/items/clip_image.tsx
+++ widgets/launcher/items/clip_image.tsx
@@ -1,11 +1,11 @@
 import Gio from "gi://Gio?version=2.0";
-import { bash } from "../../../utils/utils";
+import { bash, ensureDirectory } from "@/utils/utils";
 import { Gtk } from "ags/gtk4";
 import { timeout } from "ags/time";
 import app from "ags/gtk4/app";
 import { createState, onCleanup } from "ags";
-import { hide_all_windows } from "@/windows";
-import { config } from "@/options";
+import { hide_all_windows, windows_names } from "@/windows";
+import { config, theme } from "@/options";
 
 export function ClipImage({
    id,
@@ -15,9 +15,14 @@ export function ClipImage({
    content: RegExpMatchArray;
 }) {
    const [_, size, unit, format, width, height] = content;
-   const maxWidth = 460;
-   const widthPx = (Number(width) / Number(height)) * 100;
-   const heightPx = (105 / widthPx) * maxWidth;
+   const maxWidth =
+      config.launcher.width.get() - theme.window.padding.get() * 2;
+   let widthPx = (Number(width) / Number(height)) * 200;
+   let heightPx: number;
+
+   if (widthPx > maxWidth) heightPx = (200 / widthPx) * maxWidth;
+   else heightPx = 200;
+
    const imagePath = `/tmp/ags/cliphist/${id}.png`;
    const [image, image_set] = createState("");
    let picturebox: Gtk.Picture;
@@ -25,12 +30,9 @@ export function ClipImage({
 
    async function loadImage() {
       try {
-         await bash(`mkdir -p "/tmp/ags/cliphist/"`);
+         ensureDirectory("/tmp/ags/cliphist/");
          await bash(`cliphist decode ${id} > ${imagePath}`);
          image_set(imagePath);
-         timeout(1000, () => {
-            bash(`rm -f ${imagePath}`).catch(print);
-         });
       } catch (error) {
          console.error(`Failed to load image preview: ${error}`);
       }
@@ -42,31 +44,32 @@ export function ClipImage({
    });
 
    return (
-      <box class={"image-container"} heightRequest={heightPx}>
-         <button
-            cssClasses={["launcher-button", "clipbutton", "image-content"]}
-            hexpand
-            onClicked={() => {
-               bash(`cliphist decode ${id} | wl-copy`);
-               hide_all_windows();
-            }}
-            focusOnClick={false}
-            $={() => {
-               appconnect = app.connect("window-toggled", (_, win) => {
-                  const winName = win.name;
-                  const visible = win.visible;
+      <button
+         cssClasses={["launcher-button", "clipbutton", "image-content"]}
+         heightRequest={heightPx}
+         hexpand
+         onClicked={() => {
+            bash(`cliphist decode ${id} | wl-copy`);
+            hide_all_windows();
+         }}
+         focusOnClick={false}
+         $={() => {
+            appconnect = app.connect("window-toggled", (_, win) => {
+               const winName = win.name;
+               const visible = win.visible;
 
-                  if (winName == config.launcher.name && !visible) {
-                     picturebox.set_file(null);
-                  }
-               });
-            }}
-         >
-            <Gtk.Picture
-               $={(self) => (picturebox = self)}
-               file={image.as((p) => Gio.file_new_for_path(p))}
-            />
-         </button>
-      </box>
+               if (winName == windows_names.launcher && !visible) {
+                  picturebox.set_file(null);
+                  bash(`rm -f ${imagePath}`);
+               }
+            });
+         }}
+      >
+         <Gtk.Picture
+            halign={Gtk.Align.START}
+            $={(self) => (picturebox = self)}
+            file={image.as((p) => Gio.file_new_for_path(p))}
+         />
+      </button>
    );
 }
