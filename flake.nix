e66cc2505198b2c5616a737f15cf152de6afa801
hezaki <leronpro10@gmail.com>
2025-10-18 23:01:30 +0400

    update: flake.nix

--- flake.nix
+++ flake.nix
@@ -30,22 +30,38 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      shellBuildInputs = with pkgs; [
-        gjs
-        glib
-        gtk4
-        brightnessctl
-        dart-sass
-        gpu-screen-recorder
-        cliphist
-        bluez
-      ] ++ [
-        astal.packages.${system}.io
-        astal.packages.${system}.astal4
-        
-        astal_niri.packages.${system}.niri
-        ags.packages.${system}.agsFull
-      ];
+      shellBuildInputs =
+        with pkgs;
+        [
+          gjs
+          glib
+          gtk4
+          brightnessctl
+          dart-sass
+          gpu-screen-recorder
+          cliphist
+          bluez
+          libsoup_3
+          libadwaita
+          gnutls
+        ]
+        ++ [
+          astal.packages.${system}.io
+          astal.packages.${system}.astal4
+          astal.packages.${system}.apps
+          astal.packages.${system}.hyprland
+          astal.packages.${system}.battery
+          astal.packages.${system}.bluetooth
+          astal.packages.${system}.mpris
+          astal.packages.${system}.network
+          astal.packages.${system}.notifd
+          astal.packages.${system}.powerprofiles
+          astal.packages.${system}.tray
+          astal.packages.${system}.wireplumber
+
+          astal_niri.packages.${system}.niri
+          ags.packages.${system}.agsFull
+        ];
     in
     {
       packages.${system}.default = pkgs.stdenv.mkDerivation {
