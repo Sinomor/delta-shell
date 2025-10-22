2408ccfbbb39dc8aba9da55aeddbca426d874b9e
sinomor <scarletweel@gmail.com>
2025-10-22 23:03:39 +0300

    refactor: recordindicator baritem

--- src/modules/bar/items/recordindicator.tsx
+++ src/modules/bar/items/recordindicator.tsx
@@ -4,6 +4,7 @@ import BarItem from "@/src/widgets/baritem";
 import { createBinding } from "ags";
 import { Gtk } from "ags/gtk4";
 import { isVertical } from "../bar";
+import { icons } from "@/src/lib/icons";
 const screenRecord = ScreenRecord.get_default();
 
 export function RecordIndicator() {
@@ -13,27 +14,12 @@ export function RecordIndicator() {
          onPrimaryClick={() => screenRecord.stop().catch(() => "")}
          hexpand={isVertical}
       >
-         <box
-            spacing={theme.bar.spacing}
-            orientation={
-               isVertical
-                  ? Gtk.Orientation.VERTICAL
-                  : Gtk.Orientation.HORIZONTAL
-            }
-         >
-            <box
-               class={"record-indicator"}
-               valign={Gtk.Align.CENTER}
-               halign={Gtk.Align.CENTER}
-            />
-            <label
-               label={createBinding(screenRecord, "timer").as((time) => {
-                  const sec = time % 60;
-                  const min = Math.floor(time / 60);
-                  return `${min}:${sec < 10 ? "0" + sec : sec}`;
-               })}
-            />
-         </box>
+         <image
+            hexpand={isVertical}
+            class={"record-indicator"}
+            iconName={icons.video}
+            pixelSize={20}
+         />
       </BarItem>
    );
 }
